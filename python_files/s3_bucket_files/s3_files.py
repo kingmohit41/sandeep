e9ad0b0be24499346e532912e2467c903610f555
kingmohit41 <kingmohit41>
2024-01-24 15:59:38 +0530

    messages

---
+++ python_files/s3_bucket_files/s3_files.py
@@ -0,0 +1,35 @@
+import boto3
+import pandas as pd
+from io import BytesIO
+
+
+def read_s3_data(aws_access_key_id,aws_secret_access_key,bucket_name,prefix,conn_str,database,collection):
+    client = MongoClient(conn_str)
+    db = client[database] 
+    collection = db[collection]
+    s3_client = boto3.client('s3', aws_access_key_id=aws_access_key_id,
+                            aws_secret_access_key=aws_secret_access_key)
+
+
+    objects = s3_client.list_objects(Bucket=bucket_name, Prefix=prefix)
+
+
+    for obj in objects.get('Contents', []):
+        file_key = obj['Key']
+
+
+        if not file_key.endswith('/'):
+
+            response = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+            file_content = response['Body'].read()
+
+
+            df = pd.read_csv(BytesIO(file_content))
+            records = df.to_dict(orient='records')
+            
+            collection.insert_many(records)
+            print(f"Inserted {df.shape[0]} rows  into MongoDB")
+            logging.info(f"Inserted {df.shape[0]} rows  into MongoDB")
+
+            print(f"File: {file_key}, Shape: {df.shape}")
+            logging.info(f"File: {file_key}, Shape: {df.shape}")
