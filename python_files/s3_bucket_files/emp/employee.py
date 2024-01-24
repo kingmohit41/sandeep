2d3aab07deebf070eda0057c6fe77b6f048b080e
kingmohit41 <kingmohit41>
2024-01-24 18:35:05 +0530

    msg

---
+++ python_files/s3_bucket_files/emp/employee.py
@@ -0,0 +1,11 @@
+import random,logging
+import pandas as pd
+def emp_data():
+    l=[]
+    for i in range(20):
+        d={"emp_id":i, "department":random.choice(['IT','HR','Sales','Operations']),
+          "salary":random.randint(10000,100000),"working_mode":random.choice(['Hybrid','onsite'])}
+        logging.info(d)
+        l.append(d)
+    df=pd.DataFrame(l)
+    return df
